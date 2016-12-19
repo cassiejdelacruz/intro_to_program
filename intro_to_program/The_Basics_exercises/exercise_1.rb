abd843cffb2e633a26f0a6423421aaee6e841fc1
Cassie Delacruz <cj17@msn.com>
2016-12-19 15:17:54 -0600

    The Basics Chapter

---
+++ intro_to_program/The_Basics_exercises/exercise_1.rb
@@ -0,0 +1,2 @@
+my_name = "Cassie " + "Delacruz"
+puts my_name
---
+++ intro_to_program/The_Basics_exercises/exercise_5.rb
@@ -0,0 +1,9 @@
+factorial5 = 5 * 4 * 3 * 2 * 1
+factorial6 = 6 * 5 * 4 * 3 * 2 * 1
+factorial7 = 7 * 6 * 5 * 4 * 3 * 2 * 1
+factorial8 = 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts factorial5
+puts factorial6
+puts factorial7
+puts factorial8
