ec0c8d18883b01f8aa2e9d291db5a1489cc8547d
Cassie Delacruz <cj17@msn.com>
2016-12-20 16:03:29 -0600

    Variables Chapter

---
+++ intro_to_program/Variables_exercises/name.rb
@@ -0,0 +1,11 @@
+puts "Type your name"
+name = gets.chomp
+puts "Hi " + name + " have a great day!"
+
+10.times {|n| puts name}
+
+puts "Whats your first name"
+first_name = gets.chomp
+puts "Whats your last name"
+last_name = gets.chomp
+puts "Cool, your full name is " + first_name + " " + last_name
